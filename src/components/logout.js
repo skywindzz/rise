4f1c52f4c2e25b1cb3c71cced1d9234e251e4a13
skywindzz <skywindzz@yahoo.com>
2020-04-06 00:56:11 -0700

    fixed grid on profile page

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { navigate } from "gatsby"
+import {navigate} from "gatsby"
 import Auth from '../utils/auth.js'
 
 const auth = new Auth();
 
 const LogOut = () => {
-    {/*This component will Logout user and return them to landing page */}
+    {/*This component will Logout user and return them to landing page */
+    }
 
     return (
         <button className="loginStyle" onClick={auth.logout}>Logout</button>
     )
 }
 
-export default LogOut 
+export default LogOut
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -6,104 +6,59 @@ import {Link} from "gatsby"
 import Logout from "../components/logout"
 import {FaAutoprefixer} from "react-icons/fa"
 import Img from "gatsby-image"
+import '../style/profile.css';
 
 setConfig({pureSFC: true})
 
 const IndexPage = () => {
 
   return (
-    <div css={{
-      fontFamily: "Helvetica Neue"
-    }}>
-      <div
-        css={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr 1fr 1fr",
-        gridTemplateRows: "100vh",
-        gridTemplateAreas: ` 'profile plan habit daily' `
-      }}>
-        {/* will need to make a index page navbar which have sign in, new account on top
+    <div className='p-container'>
+      {/* will need to make a index page navbar which have sign in, new account on top
       and a faq link on bottom for intro
       it will need 3 photos each seperate the planning, habit, daily page which is
       acessable once signed in if not signed in direct to new account page */}
 
-        <div
-          css={{
-          backgroundColor: "blue",
-          gridArea: "profile",
-          display: "grid",
-          justifyItems: "center"
-        }}>
-          <span css={{
-            margin: "0 auto"
-          }}>
-            user profile
-          </span>
+      <div className='p-profile'>
+        <span>
+          user profile
+        </span>
+        <p>without text profile section somehow will collapse, need to find a solution</p>
 
-          {/*check for gatsby udemy example and fill this out  <Img /> */}
-          <span
-            css={{
-            display: "block",
-            width: "250px",
-            height: "250px",
-            borderRadius: "50%",
-            backgroundColor: "white"
-          }}></span>
-          <p css={{
-            margin: "0 auto"
-          }}>Name</p>
-          <div css={{
-            marginLeft: "-35%"
-          }}>
-            <Link to="/">
-              <Logout></Logout>
-            </Link>
-          </div>
-        </div>
-
-        <div
-          css={{
-          backgroundColor: "hotpink",
-          "&:hover": {
-            color: "lightgreen"
-          },
-          marginTop: 0,
-          marginBottom: 0,
-          marginLeft: 0,
-          marginRight: 0,
-          gridArea: "plan"
+        {/*check for gatsby udemy example and fill this out  <Img /> */}
+        <span className="userphoto"></span>
+        <p>Name</p>
+        <div css={{
+          marginLeft: "-30%"
         }}>
-          <Link to="/planning">Planning</Link>
-          <p>
-            shows what user have inputed for the planning page, encourgae user to read it
-            daily to remind them why they are doing this
-          </p>
+          <Link to="/">
+            <Logout></Logout>
+          </Link>
         </div>
+      </div>
 
-        <div
-          css={{
-          backgroundColor: "lightGreen",
-          gridArea: "habit"
-        }}>
-          <Link to="/habit">Habit page</Link>
-          <p>
-            checkboxes for tracking daily habits and also track how many consequtive days a
-            person can do the habit(maybe add some graphic for each success day
-          </p>
-        </div>
+      <div className='p-plan'>
+        <Link to="/planning">Planning</Link>
+        <p>
+          shows what user have inputed for the planning page, encourgae user to read it
+          daily to remind them why they are doing this
+        </p>
+      </div>
 
-        <div
-          css={{
-          display: "flex",
-          gridArea: "daily",
-          justifyContent: "space-evenly"
-        }}>
-          <span>Daily</span>
-          <p>
-            daily needs a component, not it's own page, people can directly do their 3D1M
-            and cross them out when done. 3 task 1 must checkboxes other minor tasks
-          </p>
-        </div>
+      <div className='p-habit'>
+        <Link to="/habit">Habit page</Link>
+        <p>
+          checkboxes for tracking daily habits and also track how many consequtive days a
+          person can do the habit(maybe add some graphic for each success day
+        </p>
+      </div>
+
+      <div className='p-daily'>
+        <span>Daily</span>
+        <p>
+          daily needs a component, not it's own page, people can directly do their 3D1M
+          and cross them out when done. 3 task 1 must checkboxes other minor tasks
+        </p>
       </div>
     </div>
   )
